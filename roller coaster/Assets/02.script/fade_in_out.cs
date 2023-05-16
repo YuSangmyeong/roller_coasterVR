4b36dcb6e0f19cf5239da01febbc1d4c5650c67b
YuSangmyeong <116189461+YuSangmyeong@users.noreply.github.com>
2023-05-16 19:21:10 +0900

    add
    add

---
+++ roller coaster/Assets/02.script/fade_in_out.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.UI;
+public class fade_in_out : MonoBehaviour
+{
+    public float FadeTime = 2f; // Fade효과 재생시간
+
+    Image fadeImg;
+
+    float start;
+
+    float end;
+
+    float time = 0f;
+
+    bool isPlaying = false;
+
+
+
+    void Awake()
+
+    {
+
+        fadeImg = GetComponent<Image>();
+
+        InStartFadeAnim();
+
+    }
+
+    public void OutStartFadeAnim()
+
+    {
+
+        if (isPlaying == true) //중복재생방지
+
+        {
+
+            return;
+
+        }
+
+        start = 1f;
+
+        end = 0f;
+
+        StartCoroutine("fadeoutplay");    //코루틴 실행
+    }
+    public void InStartFadeAnim()
+
+    {
+
+        if (isPlaying == true) //중복재생방지
+
+        {
+
+            return;
+
+        }
+
+        StartCoroutine("fadeIntanim");
+
+    }
+    IEnumerator fadeoutplay()
+
+    {
+
+        isPlaying = true;
+
+
+        UnityEngine.Color fadeColor = fadeImg.color;
+        Color fadecolor = fadeImg.color;
+        time = 0f;
+
+        Color.a = Mathf.Lerp(start, end, time);
+
+
+
+        while (fadecolor.a > 0f)
+
+        {
+
+            time += Time.deltaTime / FadeTime;
+
+            fadecolor.a = Mathf.Lerp(start, end, time);
+
+            fadeImg.color = fadecolor;
+
+            yield return null;
+
+        }
+
+        isPlaying = false;
+
+    }
+}
+
+
+
