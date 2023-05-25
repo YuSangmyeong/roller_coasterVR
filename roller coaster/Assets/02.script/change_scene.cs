b89201244f0ee726dc91f82ad0b1154c528d0f40
YuSangmyeong <116189461+YuSangmyeong@users.noreply.github.com>
2023-05-25 19:40:36 +0900

    add
    add

---
+++ roller coaster/Assets/02.script/change_scene.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change_scene : MonoBehaviour
+{
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void NextSceneWithNum()
+    {
+        if(private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+        {
+            SceneManager.LoadScene(1);
+        }
+        
+    }
+}
